c17cc4a4b12f1422571f1dc5ab320dc8ac982ce2
Chris Andaya <exmaple@email.com>
2021-10-04 00:49:38 -0700

    server deploy digital ocean

--- server/db/index.js
+++ server/db/index.js
@@ -25,7 +25,7 @@ const prodGoogleCloud = {
 }
 
 const prodDigitalOcean = {
-  host: `private-app-6bdba4b6-1366-4538-928c-5e57afca2e7a-do-user-995435.b.db.ondigitalocean.com`,
+  host: `app-6bdba4b6-1366-4538-928c-5e57afca2e7a-do-user-9954352-0.b.db.ondigitalocean.com`,
   database: 'reportr',
   user: 'reportr',
   password: 'ON8QxBRYz0eujuwD',
