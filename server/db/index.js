2b6ffbc2bcc6ae6b49b30cbe32c8d5d13cc5ee67
Chris Andaya <exmaple@email.com>
2021-10-04 00:14:56 -0700

    server deploy digital ocean

--- server/db/index.js
+++ server/db/index.js
@@ -30,7 +30,12 @@ const prodDigitalOcean = {
   user: 'reportr',
   password: 'ON8QxBRYz0eujuwD',
   port: 25060,
-  
+  sslmode: require  ,
+//   ssl: {
+//     ca   : fs.readFileSync("server-ca.pem").toString(),
+//     key  : fs.readFileSync("client-key.pem").toString(),
+//     cert : fs.readFileSync("client-cert.pem").toString(),
+// }
 }
 
 const isProd = PORT !== 3001
