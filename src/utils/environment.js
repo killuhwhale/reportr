91966fb9d9042d2ac1f997447f32b78557b4560a
Chris Andaya <chrisandaya@brain-2.local>
2022-01-11 00:17:51 -0800

    Testing

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -1,3 +1,7 @@
+
 export const isProd = window.location.hostname !== 'localhost'
 export const isTesting = true
 export const BASE_URL = isProd ? 'https://reportr-paai9.ondigitalocean.app' : 'http://localhost:3001'
+
+console.log("Environment!@!@!@!@!@@!")
+console.log(process.env)
