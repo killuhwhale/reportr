a68dd185dd97ee05482b0109915d0949eafa158d
Chris Andaya <20297127+killuhwhale@users.noreply.github.com>
2022-05-19 20:30:11 -0700

    Testing workflow

--- src/utils/environment.js
+++ src/utils/environment.js
@@ -4,3 +4,4 @@ const githubWorkflow = process.env.WORKFLOW_GITHUB_ENV
 export const isProd = window.location.hostname !== 'localhost'
 export const BASE_URL = githubWorkflow ? 'http://10.1.0.43' : isProd ? 'https://reportr-paai9.ondigitalocean.app' : 'http://localhost:3001'
 console.log(`Environment:: BASE_URL `, BASE_URL)
+console.log(process.env, process.env.WORKFLOW_GITHUB_ENV)
